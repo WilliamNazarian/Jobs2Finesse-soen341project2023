99971670b519eb2be000e081425d863a5994216c
Paul Farid <61884929+PaulFarid@users.noreply.github.com>
2023-04-18 22:38:12 -0400

    Update multerUpload.js

--- backend/middleware/multerUpload.js
+++ backend/middleware/multerUpload.js
@@ -1,5 +1,7 @@
 const multer = require('multer');
 
+// this code sets up a Multer middleware configuration that saves uploaded files to the CVs directory with a unique filename based on 
+//the current timestamp and the original filename of the uploaded file.
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'CVs');
@@ -9,6 +11,7 @@ const storage = multer.diskStorage({
   },
 });
 
+//this function allows only PDF files to be uploaded, and rejects all other file types with an error message.
 const fileFilter = (req, file, cb) => {
   if (file.mimetype === 'application/pdf') {
     cb(null, true);
@@ -17,6 +20,7 @@ const fileFilter = (req, file, cb) => {
   }
 };
 
+// this code sets up a Multer middleware instance with the specified storage and file filtering options, which can be used to handle file uploads
 const upload = multer({
   storage: storage,
   fileFilter: fileFilter,
