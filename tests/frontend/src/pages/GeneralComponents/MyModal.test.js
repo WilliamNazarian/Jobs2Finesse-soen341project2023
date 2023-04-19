f93a70d7e65efa9aded86b9dd7ac20ebc9f43953
Cleopatr-Aliak Manoukian <70761047+kaianinja@users.noreply.github.com>
2023-04-19 00:52:21 -0400

    Update MyModal.test.js

--- tests/frontend/src/pages/GeneralComponents/MyModal.test.js
+++ tests/frontend/src/pages/GeneralComponents/MyModal.test.js
@@ -1,87 +1,62 @@
 import React from "react";
-import { render, fireEvent, screen } from "@testing-library/react";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import FormInsideModal from "./FormInsideModal";
-import { submittedFormDataActions } from "../../store/submittedFormData";
-import { useSelector, useDispatch } from "react-redux";
-import { useLocation } from "react-router-dom";
+import { mount } from "enzyme";
+import MyModal from "./MyModal";
 
-// Mock Redux store
-const store = createStore(() => ({}));
+describe("MyModal", () => {
+  let wrapper;
+  const props = {
+    open: true,
+    closeModal: jest.fn(),
+  };
 
-// Mock useDispatch hook
-jest.mock("react-redux", () => ({
-  useDispatch: jest.fn(),
-  useSelector: jest.fn(),
-}));
-
-// Mock useLocation hook
-jest.mock("react-router-dom", () => ({
-  useLocation: jest.fn(),
-}));
-
-describe("FormInsideModal", () => {
   beforeEach(() => {
-    useDispatch.mockReturnValue(jest.fn());
-    useSelector.mockReturnValue("");
-    useLocation.mockReturnValue({ pathname: "/jobs" });
+    wrapper = mount(<MyModal {...props} />);
   });
 
-  test("renders form with input fields and submit button", () => {
-    render(
-      <Provider store={store}>
-        <FormInsideModal onSubmitForm={() => {}} />
-      </Provider>
-    );
-
-    // Check if form fields are rendered
-    expect(screen.getByLabelText("Company Name")).toBeInTheDocument();
-    expect(screen.getByLabelText("Number Of Positions")).toBeInTheDocument();
-    expect(screen.getByLabelText("Postion Offered")).toBeInTheDocument();
-    expect(screen.getByLabelText("Country")).toBeInTheDocument();
-    expect(screen.getByLabelText("Address")).toBeInTheDocument();
-    expect(screen.getByLabelText("Job Descripton")).toBeInTheDocument();
-
-    // Check if checkboxes are rendered
-    expect(screen.getByLabelText("Full Time:")).toBeInTheDocument();
-    expect(screen.getByLabelText("Part Time:")).toBeInTheDocument();
-    expect(screen.getByLabelText("Internship:")).toBeInTheDocument();
-
-    // Check if submit button is rendered
-    expect(screen.getByRole("button", { name: "Enter Job Post" })).toBeInTheDocument();
+  afterEach(() => {
+    wrapper.unmount();
   });
 
-  test("submit form with valid data", () => {
-    const onSubmitFormMock = jest.fn();
-    const setSubmittedFormDataMock = jest.fn();
-    useDispatch.mockReturnValueOnce(jest.fn());
-    useDispatch.mockReturnValueOnce(onSubmitFormMock);
-    useSelector.mockReturnValueOnce("test@example.com");
-    useSelector.mockReturnValueOnce(setSubmittedFormDataMock);
+  it("renders the modal with correct props", () => {
+    expect(wrapper.find("Modal").prop("open")).toBe(true);
+    expect(wrapper.find("Modal").prop("onClose")).toBe(props.closeModal);
+    expect(wrapper.find("Fade").prop("in")).toBe(true);
+  });
 
-    render(
-      <Provider store={store}>
-        <FormInsideModal onSubmitForm={onSubmitFormMock} />
-      </Provider>
-    );
+  it("renders the modal content with correct styles", () => {
+    expect(wrapper.find("Box").prop("sx")).toEqual(expect.objectContaining({
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      transform: "translate(-50%, -50%)",
+      width: 500,
+      bgcolor: expect.any(String),
+      border: `2px solid ${expect.any(String)}`,
+      boxShadow: 24,
+      p: 4,
+      borderRadius: "30px"
+    }));
+  });
 
-    // Fill in form fields
-    fireEvent.change(screen.getByLabelText("Company Name"), { target: { value: "Test Company" } });
-    fireEvent.change(screen.getByLabelText("Number Of Positions"), { target: { value: "5" } });
-    fireEvent.change(screen.getByLabelText("Postion Offered"), { target: { value: "Test Position" } });
-    fireEvent.change(screen.getByLabelText("Country"), { target: { value: "Test Country" } });
-    fireEvent.change(screen.getByLabelText("Address"), { target: { value: "Test Address" } });
-    fireEvent.change(screen.getByLabelText("Job Descripton"), { target: { value: "Test Description" } });
-    fireEvent.click(screen.getByLabelText("Full Time:"));
-    fireEvent.click(screen.getByLabelText("Part Time:"));
-    fireEvent.click(screen.getByLabelText("Internship:"));
+  it("renders the modal title with correct text and styles", () => {
+    expect(wrapper.find("Typography").text()).toBe("POST A JOB");
+    expect(wrapper.find("Typography").prop("sx")).toEqual(expect.objectContaining({
+      textAlign: "center"
+    }));
+  });
 
-    // Submit form
-    fireEvent.click(screen.getByRole("button", { name: "Enter Job Post" }));
+  it("renders the form inside the modal", () => {
+    expect(wrapper.find("FormInsideModal").exists()).toBe(true);
+  });
 
-    // Check if form is submitted
-    expect(onSubmitFormMock).toHaveBeenCalled();
-    expect(setSubmittedFormDataMock).toHaveBeenCalledWith(true);
+  it("calls closeModal prop when modal is closed", () => {
+    wrapper.find("Modal").simulate("close");
+    expect(props.closeModal).toHaveBeenCalledTimes(1);
+  });
+
+  it("calls closeModalHandler prop when form is submitted", () => {
+    const formInsideModal = wrapper.find("FormInsideModal");
+    formInsideModal.props().onSubmitForm();
+    expect(props.closeModal).toHaveBeenCalledTimes(1);
   });
 });
