b9d8fc810bb603b029fb4ec46e87b7635fceefc6
Cleopatr-Aliak Manoukian <70761047+kaianinja@users.noreply.github.com>
2023-04-19 00:54:07 -0400

    Update MyNavbar.test.js

--- tests/frontend/src/pages/GeneralComponents/MyNavbar.test.js
+++ tests/frontend/src/pages/GeneralComponents/MyNavbar.test.js
@@ -1,87 +1,78 @@
 import React from "react";
-import { render, fireEvent, screen } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
-import FormInsideModal from "./FormInsideModal";
-import { submittedFormDataActions } from "../../store/submittedFormData";
-import { useSelector, useDispatch } from "react-redux";
-import { useLocation } from "react-router-dom";
+import configureStore from "redux-mock-store";
+import MyNavbar from "./MyNavbar";
 
-// Mock Redux store
-const store = createStore(() => ({}));
-
-// Mock useDispatch hook
-jest.mock("react-redux", () => ({
-  useDispatch: jest.fn(),
-  useSelector: jest.fn(),
+// Mock the dependencies
+jest.mock("@mui/material/styles", () => ({
+  styled: jest.fn(),
+  useTheme: jest.fn(),
 }));
-
-// Mock useLocation hook
 jest.mock("react-router-dom", () => ({
-  useLocation: jest.fn(),
+  Link: jest.fn().mockReturnValue("a"),
+  useNavigate: jest.fn(),
+}));
+jest.mock("react-redux", () => ({
+  useSelector: jest.fn(),
+  useDispatch: jest.fn(),
+}));
+jest.mock("../../store/auth", () => ({
+  authActions: {
+    logout: jest.fn(),
+  },
 }));
 
-describe("FormInsideModal", () => {
+describe("MyNavbar", () => {
+  const mockStore = configureStore();
+  let store;
+  let component;
+
   beforeEach(() => {
-    useDispatch.mockReturnValue(jest.fn());
-    useSelector.mockReturnValue("");
-    useLocation.mockReturnValue({ pathname: "/jobs" });
-  });
+    store = mockStore({
+      auth: {
+        accountType: "guest",
+        firstName: "John",
+        lastName: "Doe",
+      },
+    });
 
-  test("renders form with input fields and submit button", () => {
-    render(
+    component = render(
       <Provider store={store}>
-        <FormInsideModal onSubmitForm={() => {}} />
+        <MyNavbar />
       </Provider>
     );
-
-    // Check if form fields are rendered
-    expect(screen.getByLabelText("Company Name")).toBeInTheDocument();
-    expect(screen.getByLabelText("Number Of Positions")).toBeInTheDocument();
-    expect(screen.getByLabelText("Postion Offered")).toBeInTheDocument();
-    expect(screen.getByLabelText("Country")).toBeInTheDocument();
-    expect(screen.getByLabelText("Address")).toBeInTheDocument();
-    expect(screen.getByLabelText("Job Descripton")).toBeInTheDocument();
-
-    // Check if checkboxes are rendered
-    expect(screen.getByLabelText("Full Time:")).toBeInTheDocument();
-    expect(screen.getByLabelText("Part Time:")).toBeInTheDocument();
-    expect(screen.getByLabelText("Internship:")).toBeInTheDocument();
-
-    // Check if submit button is rendered
-    expect(screen.getByRole("button", { name: "Enter Job Post" })).toBeInTheDocument();
   });
 
-  test("submit form with valid data", () => {
-    const onSubmitFormMock = jest.fn();
-    const setSubmittedFormDataMock = jest.fn();
-    useDispatch.mockReturnValueOnce(jest.fn());
-    useDispatch.mockReturnValueOnce(onSubmitFormMock);
-    useSelector.mockReturnValueOnce("test@example.com");
-    useSelector.mockReturnValueOnce(setSubmittedFormDataMock);
-
-    render(
-      <Provider store={store}>
-        <FormInsideModal onSubmitForm={onSubmitFormMock} />
-      </Provider>
-    );
-
-    // Fill in form fields
-    fireEvent.change(screen.getByLabelText("Company Name"), { target: { value: "Test Company" } });
-    fireEvent.change(screen.getByLabelText("Number Of Positions"), { target: { value: "5" } });
-    fireEvent.change(screen.getByLabelText("Postion Offered"), { target: { value: "Test Position" } });
-    fireEvent.change(screen.getByLabelText("Country"), { target: { value: "Test Country" } });
-    fireEvent.change(screen.getByLabelText("Address"), { target: { value: "Test Address" } });
-    fireEvent.change(screen.getByLabelText("Job Descripton"), { target: { value: "Test Description" } });
-    fireEvent.click(screen.getByLabelText("Full Time:"));
-    fireEvent.click(screen.getByLabelText("Part Time:"));
-    fireEvent.click(screen.getByLabelText("Internship:"));
-
-    // Submit form
-    fireEvent.click(screen.getByRole("button", { name: "Enter Job Post" }));
-
-    // Check if form is submitted
-    expect(onSubmitFormMock).toHaveBeenCalled();
-    expect(setSubmittedFormDataMock).toHaveBeenCalledWith(true);
+  test("renders logo", () => {
+    const logoElement = component.getByAltText("Logo");
+    expect(logoElement).toBeInTheDocument();
   });
+
+  test("opens user menu on click", () => {
+    const userMenuButton = component.getByLabelText("user-menu");
+    fireEvent.click(userMenuButton);
+    const menuItemElement = component.getByText("Logout");
+    expect(menuItemElement).toBeInTheDocument();
+  });
+
+  test("logs out on click logout", () => {
+    const dispatchMock = jest.fn();
+    const authActionsMock = require("../../store/auth").authActions;
+    authActionsMock.logout.mockImplementation(() => ({
+      type: "AUTH/LOGOUT",
+    }));
+    const useDispatchMock = require("react-redux").useDispatch;
+    useDispatchMock.mockReturnValue(dispatchMock);
+
+    const userMenuButton = component.getByLabelText("user-menu");
+    fireEvent.click(userMenuButton);
+    const logoutMenuItem = component.getByText("Logout");
+    fireEvent.click(logoutMenuItem);
+
+    expect(dispatchMock).toHaveBeenCalledTimes(1);
+    expect(dispatchMock).toHaveBeenCalledWith({ type: "AUTH/LOGOUT" });
+  });
+
+  // Add more tests here for other functionalities in the component
 });
