8e0eb399a59379fd0bfdcf889852af064361e8df
HelloMeFriend <80709119+HelloMeFriend@users.noreply.github.com>
2023-04-18 23:02:55 -0400

    Update applicationRoutes.js

--- backend/routes/applicationRoutes.js
+++ backend/routes/applicationRoutes.js
@@ -1,3 +1,4 @@
+// Import required modules and middlewares
 const express = require("express");
 const router = express.Router();
 const User = require("../mongooseCollections/User");
@@ -6,32 +7,38 @@ const upload = require("../middleware/multerUpload");
 const path = require("path");
 const mongoose = require("mongoose");
 
+// Route for applying to a job
 router.post("/", verifyJWT, upload.single("CV"), async (req, res) => {
   if (req.user.accountType !== "student") return res.json({ message: "unauthorized User" });
 
   const userId = req.user._id;
   try {
+    // Find the user by their ID
     const user = await User.findById(userId);
+    // Create a new object with the job ID, CV path, and cover letter
     const newAppliedJob = {
       job: req.body.jobId,
       CV: req.file.path,
       coverLetter: req.body.coverLetter,
     };
+    // Add the new job application to the user's array of applied jobs
     user.appliedJobs.push(newAppliedJob);
-    await user.save();
-    res.json({ message: "Job applied successfully" });
+    await user.save(); // Save the updated user object to the database
+    res.json({ message: "Job applied successfully" }); // Success message
   } catch (e) {
     console.log(e);
     res.status(500).json({ message: "Server error" });
   }
 });
 
+// Route for getting a list of students who have applied to a specific job
 router.get("/", verifyJWT, async (req, res) => {
   if (req.user.accountType !== "company") return res.json({ message: "unauthorized User" });
 
   const jobId = req.query.jobId;
 
   try {
+    // Use the MongoDB pipeline to find all users who have applied to the specified job
     const students = await User.aggregate([
       { $match: { "appliedJobs.job": mongoose.Types.ObjectId(jobId) } },
       { $unwind: "$appliedJobs" },
@@ -52,14 +59,16 @@ router.get("/", verifyJWT, async (req, res) => {
       },
     ]);
 
+    // If there are no applications for the specified job, send a response with an empty array and a message
     if (students.length === 0) return res.status(200).json({ students: [], message: "No Applications Are Submitted Yet" });
-    return res.status(200).json({ students: students });
+    return res.status(200).json({ students: students });// Send a response with the list of students who have applied
   } catch (error) {
-    console.error(error);
+    console.error(error); // send 500 error
     return res.status(500).json({ error: "Failed to fetch applied student" });
   }
 });
 
+// Route for downloading a CV file
 router.get("/download/CVs/:filename", (req, res) => {
   const filename = req.params.filename;
   const filePath = path.join(__dirname, "..", "CVs", filename);
