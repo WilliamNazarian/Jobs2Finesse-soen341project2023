9d8bf52b5f63299d5b605cea3db6b364f38074fa
Cleopatr-Aliak Manoukian <70761047+kaianinja@users.noreply.github.com>
2023-04-19 00:36:53 -0400

    Update Applications.test.js

--- tests/frontend/src/pages/Applications/Applications.test.js
+++ tests/frontend/src/pages/Applications/Applications.test.js
@@ -1,43 +1,125 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import Applications from "./Applications";
 
-// Mock the dependencies for the Applications component
+//testing uses JEST
+// Mock the necessary dependencies
 jest.mock("react-router-dom", () => ({
-  useSearchParams: jest.fn(() => [new URLSearchParams({ jobId: "123" })]),
+  useSearchParams: jest.fn(() => [new URLSearchParams({ jobId: "123" }), jest.fn()]),
 }));
 
-describe("Applications Component", () => {
-  // Mock the fetch API for fetching applications data
-  beforeAll(() => {
-    global.fetch = jest.fn(() =>
+describe("Applications component", () => {
+  test("renders component with application data", async () => {
+    // Mock the fetch response
+    const mockApplications = [
+      {
+        _id: "1",
+        firstName: "John",
+        lastName: "Doe",
+        email: "john.doe@example.com",
+        appliedJobs: {
+          job: {
+            companyName: "Example Company",
+            position: "Software Engineer",
+          },
+          coverLetter: "Sample cover letter",
+          CV: "sample_cv.pdf",
+        },
+      },
+    ];
+
+    jest.spyOn(global, "fetch").mockImplementation(() =>
       Promise.resolve({
-        json: () => Promise.resolve({ students: [] }),
+        ok: true,
+        json: () => Promise.resolve({ students: mockApplications }),
       })
     );
-  });
 
-  it("renders job applications correctly", async () => {
+    // Render the Applications component
     render(<Applications />);
 
-    // Verify that the component renders the expected UI elements
-    expect(screen.getByText("Job Applications")).toBeInTheDocument();
-    expect(screen.queryAllByRole("button")).toHaveLength(0);
+    // Wait for the fetch to complete
+    await screen.findByText("John Doe");
 
-    // Wait for fetch to resolve and component to re-render
-    await screen.findByRole("button");
+    // Assert that the application data is rendered
+    expect(screen.getByText("John Doe")).toBeInTheDocument();
+    expect(screen.getByText("Cover Letter")).toBeInTheDocument();
+    expect(screen.getByText("Download CV")).toBeInTheDocument();
+    expect(screen.getByText("Select For interview")).toBeInTheDocument();
+    expect(screen.getByText("Reject")).toBeInTheDocument();
 
-    // Verify that the expected API endpoint is called with the correct headers
-    expect(global.fetch).toHaveBeenCalledWith(
-      "/application?jobId=123",
-      expect.objectContaining({
-        headers: {
-          Authorization: expect.stringContaining("Bearer"),
+    // Clean up the fetch mock
+    global.fetch.mockRestore();
+  });
+
+  test("clicking on Download CV button calls downloadPdfHandler with correct application data", async () => {
+    // Mock the fetch response
+    const mockApplications = [
+      {
+        _id: "1",
+        firstName: "John",
+        lastName: "Doe",
+        email: "john.doe@example.com",
+        appliedJobs: {
+          job: {
+            companyName: "Example Company",
+            position: "Software Engineer",
+          },
+          coverLetter: "Sample cover letter",
+          CV: "sample_cv.pdf",
         },
+      },
+    ];
+
+    jest.spyOn(global, "fetch").mockImplementation(() =>
+      Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve({ students: mockApplications }),
       })
     );
 
-    // Verify that the component renders the expected UI elements after fetching data
-    expect(screen.queryByText("No applications found")).toBeInTheDocument();
+    // Mock the downloadPdfHandler
+    const downloadPdfHandler = jest.fn();
+
+    // Render the Applications component
+    render(<Applications />);
+
+    // Wait for the fetch to complete
+    await screen.findByText("John Doe");
+
+    // Click on the Download CV button
+    fireEvent.click(screen.getByText("Download CV"));
+
+    // Assert that downloadPdfHandler is called with correct application data
+    expect(downloadPdfHandler).toBeCalledWith(mockApplications[0]);
+
+    // Clean up the fetch mock
+    global.fetch.mockRestore();
   });
-});
+
+  test("clicking on Select For interview button calls interviewEmail with correct application data", async () => {
+    // Mock the fetch response
+    const mockApplications = [
+      {
+        _id: "1",
+        firstName: "John",
+        lastName: "Doe",
+        email: "john.doe@example.com",
+        appliedJobs: {
+          job: {
+            companyName: "Example Company",
+            position: "Software Engineer",
+          },
+          coverLetter: "Sample cover letter",
+          CV: "sample_cv.pdf",
+        },
+      },
+    ];
+
+    jest.spyOn(global, "fetch").mockImplementation(() =>
+      Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve({ students: mockApplications }),
+      })
+    );
+
+    //
