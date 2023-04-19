2456ac54d0f8d5ebc16e7a95bd5c6beb64597c16
Cleopatr-Aliak Manoukian <70761047+kaianinja@users.noreply.github.com>
2023-04-18 23:46:58 -0400

    Update auth.test.js

--- frontend/src/tests/auth.test.js
+++ frontend/src/tests/auth.test.js
@@ -1,1 +1,58 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer, { authActions } from "./authSlice";
+//test file for auth.js using Jest. 
 
+describe("authSlice", () => {
+  let store;
+
+  beforeEach(() => {
+    store = configureStore({
+      reducer: {
+        auth: authReducer,
+      },
+    });
+  });
+
+  test("should set credential correctly", () => {
+    const credential = {
+      firstName: "John",
+      lastName: "Doe",
+      email: "john.doe@example.com",
+      accountType: "user",
+    };
+
+    store.dispatch(authActions.setCredential(credential));
+
+    const { firstName, lastName, email, accountType } = store.getState().auth;
+
+    expect(firstName).toEqual(credential.firstName);
+    expect(lastName).toEqual(credential.lastName);
+    expect(email).toEqual(credential.email);
+    expect(accountType).toEqual(credential.accountType);
+  });
+
+  test("should logout and reset state", () => {
+    const currentState = {
+      firstName: "John",
+      lastName: "Doe",
+      email: "john.doe@example.com",
+      accountType: "user",
+    };
+
+    store = configureStore({
+      reducer: {
+        auth: authReducer,
+      },
+      preloadedState: { auth: currentState },
+    });
+
+    store.dispatch(authActions.logout());
+
+    const { firstName, lastName, email, accountType } = store.getState().auth;
+
+    expect(firstName).toBeNull();
+    expect(lastName).toBeNull();
+    expect(email).toBeNull();
+    expect(accountType).toEqual("guest");
+  });
+});
