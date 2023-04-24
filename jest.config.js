1296b825633966c271712f63efa82b79a77de813
Cleopatr-Aliak Manoukian <70761047+kaianinja@users.noreply.github.com>
2023-04-23 23:10:27 -0400

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // The test environment that Jest will use
+  testEnvironment: 'node',
+
+  // The file patterns that Jest will use to discover test files
+  testMatch: ['<rootDir>/test/**/*.js'],
+
+  // Any additional Jest configuration settings as needed
+  // ...
+};
