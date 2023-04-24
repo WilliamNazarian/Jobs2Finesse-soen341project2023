55742380074ad6e364efba2f8308f6d8b4f5b8e2
Cleopatr-Aliak Manoukian <70761047+kaianinja@users.noreply.github.com>
2023-04-23 23:17:21 -0400

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   testEnvironment: 'node',
 
   // The file patterns that Jest will use to discover test files
-  testMatch: ['<rootDir>/test/**/*.js'],
+  testMatch: ['../tests/**/*.js'],
 
   // Any additional Jest configuration settings as needed
   // ...
